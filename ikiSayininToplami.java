b800fdac3e8cae012ddcf05693b2b58801d4ae87
elifsara <elif.sara442@gmail.com>
2024-01-17 00:04:54 +0300

    Methodlar Çalışıldı

---
+++ ikiSayininToplami.java
@@ -0,0 +1,12 @@
+public class ikiSayininToplami {
+
+    public static int Toplama(int a, int b){
+        int sum = a + b;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Toplama(3, 5));
+    }
+    
+}
